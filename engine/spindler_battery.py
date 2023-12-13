cfe982f14cf95fc7f0eea4a624c02520bf46aef5
rukkyme <communicatejoy@yhaoo.com>
2023-12-13 13:15:16 +0100

    upgrade spindler batteries

--- engine/spindler_battery.py
+++ engine/spindler_battery.py
@@ -10,7 +10,7 @@ class SpindlerBattery(Car, ABC):
         self.current_date = current_date
         
     def battery_should_be_serviced(self):
-        if self.current_date - self.last_service_date > 2:
+        if self.current_date - self.last_service_date > 3:
             return True
         else:
             return False
