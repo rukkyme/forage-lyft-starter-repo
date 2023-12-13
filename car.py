2ce5b5e1219f3784a1ae1476f46e58b07f7f33cf
rukkyme <communicatejoy@yhaoo.com>
2023-12-13 15:05:56 +0100

    adding a criteria

--- car.py
+++ car.py
@@ -2,11 +2,13 @@
 from abc import ABC, abstractmethod
 
 class car(ABC):
-    def __init__(self, last_service_date, last_service_mileage, warning_light_is_on):
+    def __init__(self, last_service_date, last_service_mileage, warning_light_is_on, tire_wear_array):
         self.last_service_date = last_service_date
         self.last_service_mileage = last_service_mileage
         self.warning_light_is_on = warning_light_is_on
-
+        self.tire_wear_array = tire_wear_array
+    
+    
     @abstractmethod
     def needs_service(self):
         pass
